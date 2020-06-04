f13f38671649dc7b8957e2e40fb65d0f67c0ab23
Bratin Ghosh <ghosh.bratin13@gmail.com>
2020-06-04 14:15:52 +0800

    Replaced JSON file with MongoDB using Mongoose ODM

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,121 +1,112 @@
 var express = require('express');
 var router = express.Router();
-var uuid = require('uuid');
-var fs = require('fs');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/todolist', { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('Connected to MongoDB!'))
+        .catch((err) => console.log(err));
+
+/* Database initialization */
+var taskSchema = new mongoose.Schema({
+  name: String,
+  endDate: String,
+  description: String,
+  priority: Boolean,
+  days_left: Number,
+}, { collection: 'task' });
+var taskModel = mongoose.model('task', taskSchema);
+/* End of database initialization */
 
 /* Helper functions */
-var tasklist=JSON.parse(fs.readFileSync('tasklist.json', 'utf8'));
-
-function write_to_file(tasklist) {
-  fs.writeFileSync('tasklist.json', JSON.stringify(tasklist), 'utf8');
-}
 
 /* Returns the days left of each task in order */
-function to_days(tasklist) {
-  for(task of tasklist) {
-    task.days_left = parseInt(((new Date(task.endDate)) - (new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()))) / (1000 * 60 * 60 * 24), 10);
-  }
-  return tasklist;
+function to_days(endDate) {
+    days_left = parseInt(((new Date(endDate)) - (new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()))) / (1000 * 60 * 60 * 24), 10);
+  return days_left;
 }
 
 /* Sorts the tasks by due date */
-function sort(tasklist) {
-  tasklist.sort((x, y) => (x.days_left > y.days_left) ? 1 : -1);
-  tasklist.sort((x, y) => (x.priority < y.priority) ? 1 : -1);
-  return tasklist;
+function sort(tasks) {
+  tasks.sort((x, y) => (x.days_left > y.days_left) ? 1 : -1);
+  tasks.sort((x, y) => (x.priority < y.priority) ? 1 : -1);
+  return tasks;
 }
 /* End of helper functions */
 
 /* Get all the tasks. */
 router.get('/', (req, res, next) => {
-  res.render('tasks', { tasks: sort(to_days(tasklist)) });
+  taskModel.find()
+           .then(tasks => {
+             res.render('tasks', { tasks: sort(tasks) });
+           });
 });
 
-/* Directs to the page with a form for new task. */
+/* Directs to the page with a form for new task */
 router.get('/add', (req, res, next) => {
   res.render('newtask', {});
 });
 
-/* Accepts the form and adds a new task to the tasklist. */
+/* Accepts the form and adds a new task to the database */
 router.post('/add', (req, res, next) => {
   var newTask = {
-    id: uuid.v4(),
     name: req.body.name,
     endDate: req.body.endDate,
     description: req.body.description,
     priority: false,
+    days_left: to_days(req.body.endDate)
   };
 
-  tasklist.push(newTask);
-  write_to_file(tasklist);
-  res.render('tasks', { tasks: sort(to_days(tasklist)) });
+  new taskModel(newTask).save();
+  res.redirect('/tasks');
 });
 
-/* Delete task(s) from the tasklist. */
+/* Delete task(s) from the database */
 router.post('/', (req, res, next) => {
   if(typeof req.body.ids == 'object') {
     for(id of req.body.ids) {
-      tasklist = tasklist.filter(elem => elem.id !== id);
+      taskModel.findByIdAndDelete(id).exec();
     }
   }
   else {
-    tasklist = tasklist.filter(elem => elem.id !== req.body.ids);
+    taskModel.findByIdAndDelete(req.body.ids).exec();
   }
-  write_to_file(tasklist);
-  res.render('tasks', { tasks: sort(to_days(tasklist)) });
+  res.redirect('/tasks');
 });
 
 /* Get single task */
 router.get('/:id', (req, res, next) => {
-  var found = false;
-  var taskHit = {};
-  for(task of tasklist) {
-    if(task.id === req.params.id) {
-      found = true;
-      taskHit = task;
-    }
-  }
-  if(found) {
-    res.render('task', {task: taskHit});
-  }
-  else {
-    res.render('taskNotFoundError', {});
-  }
+  taskModel.findById(req.params.id)
+           .then(task => {
+             res.render('task', {task: task});
+           })
+           .catch(err => {
+            res.render('taskNotFoundError', {});
+           });
 });
 
 /* Update Description of a task */
 router.post('/:id', (req, res, next) => {
-  tasklistTemp = [];
-  taskHit = {};
-  for(task of tasklist) {
-    taskTemp = task;
-    if(taskTemp.id === req.params.id) {
-      taskTemp.description = req.body.description;
-      taskHit = taskTemp;
-    }
-    tasklistTemp.push(taskTemp);
-  }
-  tasklist = tasklistTemp;
-  write_to_file(tasklist);
-  res.render('task', {task: taskHit});
+  taskModel.findById(req.params.id)
+           .then(task => {
+             task.description = req.body.description;
+             task.save();
+             res.render('task', {task: task});
+           })
+           .catch(err => {
+            res.render('taskNotFoundError', {});
+           });
 });
 
 /* Toggle Priority of a task */
 router.post('/:id/priority', (req, res, next) => {
-  tasklistTemp = [];
-  taskHit = {};
-  for(task of tasklist) {
-    taskTemp = task;
-    if(taskTemp.id === req.params.id) {
-      taskTemp.priority = !taskTemp.priority;
-      taskHit = taskTemp;
-    }
-    tasklistTemp.push(taskTemp);
-  }
-  tasklist = tasklistTemp;
-  console.log(taskHit)
-  write_to_file(tasklist);
-  res.render('task', {task: taskHit});
+  taskModel.findById(req.params.id)
+           .then(task => {
+             task.priority = !task.priority;
+             task.save();
+             res.render('task', {task: task});
+           })
+           .catch(err => {
+            res.render('taskNotFoundError', {});
+           });
 });
 
 module.exports = router;
